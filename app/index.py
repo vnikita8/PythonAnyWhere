7a32421723eb2398e483cb9beb86389e84b485c5
Nikita <56808423+vnikita8@users.noreply.github.com>
2023-01-21 23:28:01 +0700

    1

---
+++ app/index.py
@@ -0,0 +1,47 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+from app import app
+from pages import home
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+@app.callback(
+    Output('output_div', 'children'),
+    Input("main_button", "n_clicks"),
+    State('smoking', 'value'),
+    State('IL1b', 'value'),
+    State('TNF', 'value'),
+)
+
+def update_output(clicks, *kwargs):
+    if clicks is not None:
+        print(kwargs)
+        return f"курение: {kwargs[0]}"
+    else:
+        return "Error"
+
+
+
+
+
+
+
+
+
+
+
+
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+
+def display_page(pathname):
+    if pathname == '/home' or pathname == '/':
+        return home.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ app/pages/home.py
@@ -0,0 +1,59 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+
+style_for_block = {'display':"flex", "align-items": "center", "gap": 10}
+style_for_DropDown = {"width": 85}
+style_for_MainButton = {"width": 100}
+
+
+layout = html.Div([
+    html.H1('Кафедра Генетики Кемеровского государственного университета'),
+    
+    #Блок с курением
+    html.Div([
+        html.P('Курение:'),
+        dcc.Dropdown(["Да", "Нет"],"Нет", style=style_for_DropDown, id="smoking")
+    ], style=style_for_block),
+
+    #Блоки с генами 
+    html.Div([
+        html.P('IL1b:'),
+        dcc.Dropdown(["T/T", "T/C","C/C"],"T/T", style=style_for_DropDown, id="IL1b")
+    ], style=style_for_block),
+    html.Div([
+        html.P('TNF:'),
+        dcc.Dropdown(["G/G", "G/A", "A/A"],"G/G", style=style_for_DropDown, id="TNF")
+    ], style=style_for_block),
+    html.Div([
+        html.P('APEX1:'),
+        dcc.Dropdown(["G/G", "T/T", "T/G"],"G/G", style=style_for_DropDown, id="APEX1")
+    ], style=style_for_block),
+    html.Div([
+        html.P('XPD:'),
+        dcc.Dropdown(["T/G", "G/G", "T/T"],"T/G", style=style_for_DropDown, id="XPD")
+    ], style=style_for_block),
+    html.Div([
+        html.P('EGFR:'),
+        dcc.Dropdown(["A/A", "T/T", "A/T"],"A/A", style=style_for_DropDown, id="EGFR")
+    ], style=style_for_block),
+    html.Div([
+        html.P('CHEK2:'),
+        dcc.Dropdown(["N/P", "P/P", "N/N"],"N/P", style=style_for_DropDown, id="CHEK2")
+    ], style=style_for_block),
+    html.Div([
+        html.P('TGFb1:'),
+        dcc.Dropdown(["G/G", "G/C", "C/C"],"G/G", style=style_for_DropDown, id="TGFb1")
+    ], style=style_for_block),
+    html.Div([
+        html.P('EPHX1:'),
+        dcc.Dropdown(["T/T", "T/C", "C/C"],"T/T", style=style_for_DropDown, id="EPHX1")
+    ], style=style_for_block),
+
+    html.Button("Прогноз..", n_clicks=0, id="main_button", style=style_for_MainButton),
+    html.Div(id="output_div", children='Enter a value and press submit')
+
+], style={"align-items": "center"})
+
+
